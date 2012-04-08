1c2c3d5956de0ed237bbc0316acd39125b48c3d5
Raphael Eidus <raphaeleidus@gmail.com>
2012-04-08 19:06:44 -0400

    scaffolded LogEntry, Log, and Project

--- javascripts/observr.coffee.js
+++ javascripts/observr.coffee.js
@@ -1,4 +1,36 @@
-$ () ->
- alert "Coffee script is working"
+code = """alert "hello" """
+# code = """
+jQuery ->
+	class LogEntry extends Backbone.Model
+		defaults:
+			Start: null
+			End: null
+			Duration: null
+			Description: ""
+			Activity: null
+	class Log extends Backbone.Collection
+		model: LogEntry
+			
+	class Project extends Backbone.Model
+		defaults:
+			StartClocktime: null
+			Start: null
+			Description: "Sample Description"
+			Title: "Sample Project"
+			Activities: []
+		initialize: ->
+			# @set "Log" new Log
+			console.log @get "Activities"
+		addActivity: (activity) ->
+			Activities = @get "Activities"
+			Activities.push activity
+			@set "Activities", Activities
+	
+	project = new Project 
+		Title: "Observer"
+		Activities:  ["Watch", "Listen", "Do"]
 
-console.log CoffeeScript.compile("chain")
+	project.addActivity "Snack"
+	console.log project.get "Activities"
+# """
+console.log CoffeeScript.compile code
