b83119da91429f08d195c98c02c076218a493d2f
Raphael Eidus <raphaeleidus@gmail.com>
2012-04-30 19:42:03 -0400

    jquery typo fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,6 @@
-var DataSource = new Firebase('http://gamma.firebase.com/codercub/');
-DataSource.set('I am now writing data into Firebase!');
+$(function(){}
+	var DataSource = new Firebase('http://gamma.firebase.com/codercub/');
+	DataSource.set('I am now writing data into Firebase!');
 
-console.log("done.");
+	console.log("done.");
+});
