027f73166d691296d50f81a4f80891745befe202
Raphael Eidus <raphaeleidus@gmail.com>
2012-04-30 20:06:32 -0400

    underscore and backbone

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,15 @@
-$(function(){}
+$(function(){
+	var LogEntry = Backbone.Model.extend({
+
+	});
+	var Log = Backbone.Collection.extend({
+		model: LogEntry
+	});
+	activityLog = new Log();
+	console.log(activityLog.toJSON());
 	var dataRef = new Firebase('http://gamma.firebase.com/codercub/');
 	dataRef.on('value', function(snapshot) {
-		console.log(snapshot);
+		console.log(snapshot.val());
 	});
 	console.log("done.");
 });
