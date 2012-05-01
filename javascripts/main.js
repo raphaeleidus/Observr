62988cf8364ca2902fb5dfa9078f770677fa7475
Raphael Eidus <raphaeleidus@gmail.com>
2012-05-01 10:01:47 -0400

    mood logging

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,9 @@ $(function(){
 	var LogEntry = Backbone.Model.extend({
 
 	});
-	console.log("done.");
+	var MoodEntry = Backbone.Model.extend({
+
+	});
 	var Log = Backbone.Collection.extend({
 		model: LogEntry,
 		url: "/log",
@@ -14,6 +16,18 @@ $(function(){
 			log.load();
 		}
 	});
+	var MoodLog = Backbone.Collection.extend({
+		model: MoodEntry,
+		url: "/moodLog",
+		initialize: function() {
+			this.backboneFirebase = new BackboneFirebase(this);
+			this.on("all", this.reloadView, this);
+		},
+		reloadView: function() {
+			log.loadMood();
+		}
+	});
+	moodLog = new MoodLog();
 	activityLog = new Log();
 	var LogEntryView = Backbone.View.extend({
 		className: "logEntry",
@@ -35,21 +49,50 @@ $(function(){
 			});
 			return this;
 		},
+		renderMoods: function() {
+			var that = this;
+			//this.$el.empty().remove();
+			moodLog.each(function(moodEntry){
+				var moodEntryView = new MoodEntryView({model: moodEntry});
+				that.$el.append(moodEntryView.render());
+			});
+			//return this;
+		},
 		initialize: function() {
-			_.bindAll(this, 'addLogEntry');
-			$(document).bind('keypress', this.addLogEntry);
+			_.bindAll(this, 'catchKey');
+			$(document).bind('keydown', this.catchKey);
 		},
 		load: function(){
 			$("#activitylog").empty();
 			$("#activitylog").append(this.render().el);
 		},
-		addLogEntry: function(e) {
-			console.log(e.type, e.keyCode);
+		loadMood: function(){
+			$("#moodlog").empty();
+			$("#moodlog").append(this.renderMoods().el);
+		},
+		catchKey: function(e) {
+			if (e.keyCode === 9) {
+				e.preventDefault();
+				if(e.target === $("#activityField")[0]) {
+					$("#moodField").focus();
+				} else if(e.target === $("#moodField")[0]) {
+					$("#activityField").focus();
+				}
+			} 
 			if (e.keyCode === 13 && e.target === $("#activityField")[0] && $("#activityField").val().length > 3) {
 				entry = $("#activityField").val();
 				time = new Date();
 				activityLog.create({Title: entry, Timestamp: time.toUTCString()});
 				activityLog.fetch();
+				console.log(time.toUTCString(), entry);
+				$("#activityField").val("")
+				;
+			}
+			if (e.keyCode === 13 && e.target === $("#moodField")[0]) {
+				entry = $("#activityField").val();
+				time = new Date();
+				moodLog.create({Title: entry, Timestamp: time.toUTCString()});
+				//moodLog.fetch();
 				console.log(time.toUTCString(), entry);
 				$("#activityField").val("")
 				;
