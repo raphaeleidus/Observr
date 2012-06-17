818c398bb9fb6b811f1f349df65aa7e3d1d1462d
Raphael Eidus <raphaeleidus@gmail.com>
2012-06-16 20:43:46 -0400

    created rails project

---
+++ Observr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Observr::Application.config.secret_token = 'e404e93f2dd2772b8d1ee2e989608a4e35fbef4948f655b3bfcd14bd1235b1702c661cee8b9765ab636bcf036dc7f0d6194cd7273836fcb203a9d210d1ded90a'
---
+++ Observr/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Observr::Application.config.session_store :cookie_store, key: '_Observr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Observr::Application.config.session_store :active_record_store
